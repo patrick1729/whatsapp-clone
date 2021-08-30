0fb9678951d11e7f9bdabc4ffe73db6c7dfed580
Prateek Mishra <prateek.m046@gmail.com>
2021-08-30 13:22:49 +0530

    refactor: Rename apps' name

--- apps/whatsapp-ui/jest.config.js
+++ apps/whatsapp-ui/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'whatsapp-clone',
+  displayName: 'whatsapp-ui',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/whatsapp-clone',
+  coverageDirectory: '../../coverage/apps/whatsapp-ui',
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular',
   },
